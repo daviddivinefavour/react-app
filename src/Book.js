cd17b2e4538cea813d928f8bff8c15bc98afc8b0
Divinefavour David <blazer0022@gmail.com>
2023-08-10 06:18:33 +0100

    created book component

---
+++ src/Book.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Book = () => (
+  <article className="book">
+    <img
+      src="https://www.amazon.com/Iron-Flame-Empyrean-Rebecca-Yarros/dp/1649374178/ref=zg_bs_g_books_sccl_4/140-1169731-5021109?psc=1"
+      alt="Iron Flame (The Empyrean, 2)"
+    />
+    <h1> Iron Flame (The Empyrean, 2)</h1>
+    <h4>Rebecca Yarros</h4>
+  </article>
+)
+
+export default Book
